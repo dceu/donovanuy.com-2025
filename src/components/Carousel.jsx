5432fddd46000e0f85cfe06702007e385702c205
Donovan Conrad E. Uy <34298986+dceu@users.noreply.github.com>
2025-01-12 14:19:28 -0800

    git init

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+
+export function Photos(images, rotations){
+    let rotations = ['rotate-2', '-rotate-2', 'rotate-2', 'rotate-2', '-rotate-2']
+    
+    return (
+        <div className="mt-16 sm:mt-20">
+            {images.map((image, index) => (
+                <div
+                key={image.src}
+                className={clsx(
+                    'relative aspect-[9/10] w-44 flex-none overflow-overlay rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl',
+                    rotations ? rotations[index % rotations.length] : 'rotate-0', 
+                )}
+                >
+                    <Image
+                    src={image}
+                    alt=""
+                    sizes="(min-width: 640px) 18rem, 11rem"
+                    className="absolute inset-0 h-full w-full object-cover"
+                    />
+                </div>
+            ))}
+        </div>
+    )
+}
+    
